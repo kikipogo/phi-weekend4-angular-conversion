ba8721fd35062751ba014e4cbbf1e33a39850b2e
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-09 10:48:55 -0600

    http moved to factories

--- public/scripts/factories/task.factory.js
+++ public/scripts/factories/task.factory.js
@@ -1,6 +1,8 @@
 myApp.factory('TaskFactory', ['$http', function($http) {
 
   var factoryTasks = { list: [] };
+  var self = this;
+  self.newTask = {};
 
   getTasks();
 
@@ -12,15 +14,56 @@ myApp.factory('TaskFactory', ['$http', function($http) {
       console.log('response from factory: ', response);
       console.log('response.data from factory: ', response.data);
       factoryTasks.list = response.data;
-      // factoryTasks = {
-      //   list: [{name: 'sleep', id: 1}, {name: 'wake up', id: 2}]
-      // }
     });
   }
+
+  function completeTask(taskId){
+  // $http request moves to factory because its the glue between the factory and the server
+    $http({
+      method: 'PUT',
+      url: '/tasks/complete/' + taskId
+    }).then(function(response) {
+      getTasks();
+    });
+}
+
+function deleteTask(taskId){
+$http({
+  method: 'DELETE',
+  url: '/tasks/' + taskId
+}).then(function(response) {
+  getTasks();
+});
+}
+
+function uncompleteTask(taskId){
+$http({
+  method: 'PUT',
+  url: '/tasks/uncomplete/' + taskId
+}).then(function(response) {
+  getTasks();
+});
+}
+
+function addTask(){
+$http({
+  method: 'POST',
+  url: '/tasks',
+  data: self.newTask
+}).then(function(response){
+  console.log(response);
+  getTasks();
+  self.newTask = {};
+});
+}
 
   // this is the public API, if it's not in here, your controller won't see it
   return {
     allTasks: factoryTasks,
-    updateTasks: getTasks
+    updateTasks: getTasks,
+    completeTask: completeTask,
+    deleteTask: deleteTask,
+    uncompleteTask: uncompleteTask,
+    addTask: addTask
   };
 }]);
